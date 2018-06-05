8a61b2dd34f57f7deaeb763a196efbb8e8774a1b
James <jamesgood626@gmail.com>
2018-06-05 06:01:40 -0700

    .

--- django_crypto/django_crypto/settings.py
+++ django_crypto/django_crypto/settings.py
@@ -156,7 +156,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 REACT_APP_DIR = os.path.join(BASE_DIR, 'react')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
